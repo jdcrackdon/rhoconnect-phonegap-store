3cc7d96f7f1861b500e49f4e65e032c2eb21a5de
lars <larsburgess@gmail.com>
2011-06-23 11:22:01 -0700

    Initial Commit

---
+++ StoreSample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StoreSample
+//
+//  Created by lars on 6/23/11.
+//  Copyright Home 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
